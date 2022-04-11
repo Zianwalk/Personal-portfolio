2e4f856f45800231c6c4b345b7c5ff47212e8d67
Zianwalk <102133822+Zianwalk@users.noreply.github.com>
2022-04-11 23:26:40 +0800

    Update Pointer--1.cpp

--- C Implement--Thanks for Interview/Pointer--1.cpp
+++ C Implement--Thanks for Interview/Pointer--1.cpp
@@ -5,8 +5,10 @@ int main()
     char arr[10] = { '1', '2', '3' } ;
     char * ptr = &arr[1] ;
     
-    printf("%s\n", arr) ;
-    printf("%s\n", ptr) ;
+    printf("%s\n", arr) ; // cout << "123" ;  
+    printf("%s\n", ptr) ; // cout << "23" ;
+    
+    // endl of course //
     
     unsigned int l0, l1, l2 ;
     unsigned int * temp = &l0 ;
@@ -15,10 +17,13 @@ int main()
     l1 = l0 << 2 ;
     l2 = l1 << 8 ;
     
-    printf( "This is l0 :%d\n", l0 ) ;
-    printf( "This is l1 :%d\n", l1 ) ;
-    printf( "This is l2 :%d\n", l2 ) ;
-    printf( "This is temp :%d\n", *temp ) ;
-
+    printf( "This is l0 :%d\n", l0 ) ; // cout << 1 ;
+    printf( "This is l1 :%d\n", l1 ) ; // cout << 4 ;
+    printf( "This is l2 :%d\n", l2 ) ; // cout << 1024 ;
+    printf( "This is temp :%d\n", *temp ) ; // cout << 1 ;
+    
+    l2 = l2 | ( *temp ) ;
+    printf( "This is l2 :%d\n", l2 ) ; // cout << 1025 ;
+    
     return 0;
 }
